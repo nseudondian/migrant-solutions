95727dea6f109cdd8c663fb50cb8e21c82c3d5b7
Nseabasi Udondian <nseabasiudondian@gmail.com>
2021-11-26 15:52:16 +0100

    'migrant'

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors:{
+      purple : '#531ab2',
+      blue: '#1752e5',
+      green : '#03b994',
+      lightblue: '#F0F8FF',
+      gradient: 'rgb(23,82,229)'
+    },
+    fontFamily:{
+      'ham' :[
+        'Harmattan', 'sans-serif'
+      ]
+    },
+    container: {
+      center: true,
+      padding: '4.5rem'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
